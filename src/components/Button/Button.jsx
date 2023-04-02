8577e28ff6a210219f4c0110c98ef1a68f12a525
Andrey <108063187+Minaht007@users.noreply.github.com>
2023-04-02 16:08:11 +0300

    style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,11 @@
+import Style from '../Style/Style.module.css';
 export default function Buttom({ click }) {
   return (
-    <button type="button" onClick={() => click()}>
+    <button
+      className={(Style.SearchForm_button, Style.SearchForm_button_label)}
+      type="button"
+      onClick={() => click()}
+    >
       loadmore
     </button>
   );
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,13 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
+import Style from '../Style/Style.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 export class ModalImg extends Component {
   render() {
     const { largPhoto, closeModal } = this.props;
     return createPortal(
-      <div>
+      <div className={(Style.Modal, Style.Overlay)}>
         <img src={largPhoto} alt={largPhoto} onClick={() => closeModal()} />
       </div>,
       modalRoot
